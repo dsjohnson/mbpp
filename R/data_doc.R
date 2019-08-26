76beb30a10e6ed3facf16283c843eeab5a355cf8
Devin <devin.johnson@noaa.gov>
2019-08-26 11:59:49 -0700

    another commit
    Signed-off-by: Devin <devin.johnson@noaa.gov>

---
+++ R/data_doc.R
@@ -0,0 +1,58 @@
+#' Northern fur seal shear marking data from 2016
+#'
+#'
+#' @name nfs_mark
+#' @docType data
+#' @format
+#'
+#' A data frame with 19 observations on the following 6 variables.
+#'
+#' \describe{
+#' \item{icode}{Island designation}
+#'
+#' \item{rcode}{Rookery designation}
+#'
+#' \item{deadpups}{Number of dead pups counted on survey}
+#'
+#' \item{pupsborn}{Estimated pup production using design-based method}
+#'
+#' \item{sep}{Estimated standard error using design-based method}
+#'
+#' \item{M}{Number of pups shear marked}
+#' }
+#'
+#' @source Marine Mammal Laboratory, Alaska
+#' Fisheries Science Center, National Marine Fisheries Service, NOAA 7600 Sand
+#' Point Way NE Seattle, WA 98115
+#' @keywords datasets
+NULL
+
+
+#' Northern fur seal mark resight data from 2016
+#'
+#'
+#' @name nfs_resight
+#' @docType data
+#' @format
+#'
+#' A data frame with 76 observations on the following 6 variables.
+#'
+#' \describe{
+#' \item{icode}{Island designation}
+#'
+#' \item{rcode}{Rookery designation}
+#'
+#' \item{resample}{The resight occasion designation}
+#'
+#' \item{observer}{The observer designation}
+#'
+#' \item{m}{Number of marked pups observed}
+#'
+#' \item{u}{Number of unmarked pups observed}
+#' }
+#'
+#' @source Marine Mammal Laboratory, Alaska
+#' Fisheries Science Center, National Marine Fisheries Service, NOAA 7600 Sand
+#' Point Way NE Seattle, WA 98115
+#' @keywords datasets
+NULL
